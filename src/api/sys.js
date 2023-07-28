7c8ae04dc300bcdeb008f3f85870b39661c7bc3a
zack zheng <zack_zhengxiyun@163.com>
2023-07-29 00:20:17 +0800

    wip: 增加接口

--- src/api/sys.js
+++ src/api/sys.js
@@ -1,8 +1,18 @@
 import request from '@/utils/request'
 
+// 登陆
 export const login = data => {
   return request({
     url: '/sys/login',
+    method: 'POST',
+    data
+  })
+}
+
+// 获取用户信息
+export const getUserInfo = (data) => {
+  return request({
+    url: '/sys/profile',
     method: 'POST',
     data
   })
