f70528ea493fe026a2bec94e70088146f93e7169
zack zheng <zack_zhengxiyun@163.com>
2023-08-03 00:11:06 +0800

    wip: 用户信息

--- src/permission.js
+++ src/permission.js
@@ -12,12 +12,16 @@ const whiteList = [
 ]
 
 // 路由前置守卫
-router.beforeEach((to, from, next) => {
+router.beforeEach(async (to, from, next) => {
   if (store.getters.token) {
     // 用户已登陆
     if (to.path === '/login') {
       next('/')
     } else {
+      // 判断用户信息是否存在，如果不存在，则获取用户信息
+      if (!store.getters.hasUserInfo) {
+        await store.dispatch('user/getUserInfo')
+      }
       next()
     }
   } else {
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,6 +1,7 @@
 
 const getters = {
-  token: state => state.user.token
+  token: state => state.user.token,
+  hasUserInfo: state => JSON.stringify(state.user.userInfo) !== '{}'
 }
 
 export default getters
