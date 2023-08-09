a0de78bae89227f0b741ffd6de4cb6e2c0b545e4
zack zheng <zack_zhengxiyun@163.com>
2023-08-09 22:42:25 +0800

    wip: user info

--- src/layout/components/NavBar.vue
+++ src/layout/components/NavBar.vue
@@ -1,13 +1,61 @@
+import { ref } from 'vue';
 <template>
-  <div>
-    navbar
+  <div class="navbar">
+    <div class="right-menu">
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar shape="square" :size="40" :src="$store.getters.userInfo.avatar">
+          </el-avatar>
+          <el-icon :size="20">
+            <Tools />
+          </el-icon>
+        </div>
+        <template #dropdrown>
+          <el-dropdown-menu class="user-dropdown ">
+            <router-link to="/">
+              <el-dropdown-item>主页</el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="#">
+              <el-dropdown-item>课程主页</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided>退出登陆 </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
   </div>
 </template>
 
 <script setup>
-
+import { Tools } from '@element-plus/icons-vue'
 </script>
 
 <style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background-color: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+}
 
+.right-menu {
+  display: flex;
+  align-items: center;
+  float: right;
+  padding-right: 16px;
+}
+
+::v-deep .avatar-container {
+  cursor: pointer;
+  .avatar-wrapper {
+    margin-top: 5px;
+    position: relative;
+    .el-avatar {
+      --el-avatar-background-color: none;
+      margin-right: 12px ;
+    }
+  }
+}
 </style>
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,7 +1,8 @@
 
 const getters = {
   token: state => state.user.token,
-  hasUserInfo: state => JSON.stringify(state.user.userInfo) !== '{}'
+  hasUserInfo: state => JSON.stringify(state.user.userInfo) !== '{}',
+  userInfo: state => state.user.userInfo
 }
 
 export default getters
