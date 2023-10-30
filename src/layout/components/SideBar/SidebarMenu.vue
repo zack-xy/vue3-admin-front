c172e01c89f63c7d46380ceb3726254c171577d4
zack zheng <zack_zhengxiyun@163.com>
2023-10-30 23:41:54 +0800

    feat(function): default active menu

--- src/layout/components/SideBar/SidebarMenu.vue
+++ src/layout/components/SideBar/SidebarMenu.vue
@@ -1,23 +1,12 @@
 <template>
   <el-menu
+    :default-active="activeMenu"
     :unique-opened="true"
-    default-active="2"
-    background-color="545c64"
-    text-color="#fff"
-    active-text-color="#ffd04b"
+    :background-color="$store.getters.cssVar.menuBg"
+    :text-color="$store.getters.cssVar.menuText"
+    :active-text-color="$store.getters.cssVar.menuActiveText"
+    router
   >
-    <!-- <el-sub-menu index="1">
-      <template #title>
-        <el-icon><Location /></el-icon>
-        <span>导航一</span>
-      </template>
-      <el-menu-item index="1-1">选项一</el-menu-item>
-      <el-menu-item index="1-2">选项二</el-menu-item>
-    </el-sub-menu>
-    <el-menu-item index="2">
-      <el-icon><Location /></el-icon>
-      <template #title>导航四</template>
-    </el-menu-item> -->
     <sidebar-item
       v-for="item in routes"
       :key="item.path"
@@ -27,8 +16,7 @@
 </template>
 
 <script setup>
-// import { Location } from '@element-plus/icons-vue'
-import { useRouter } from 'vue-router'
+import { useRoute, useRouter } from 'vue-router'
 import { computed } from 'vue'
 import { filterRoutes, generateMenus } from '@/utils/route'
 import SidebarItem from './SidebarItem.vue'
@@ -40,7 +28,14 @@ const routes = computed(() => {
   return generateMenus(filteredRoutes)
 })
 
-console.log(routes.value)
+// console.log(routes.value)
+
+// 默认激活项
+const route = useRoute()
+const activeMenu = computed(() => {
+  const { path } = route
+  return path
+})
 
 </script>
 
--- src/store/getters.js
+++ src/store/getters.js
@@ -1,8 +1,10 @@
+import variables from '@/style/variables.module.scss'
 
 const getters = {
   token: state => state.user.token,
   hasUserInfo: state => JSON.stringify(state.user.userInfo) !== '{}',
-  userInfo: state => state.user.userInfo
+  userInfo: state => state.user.userInfo,
+  cssVar: state => variables
 }
 
 export default getters
