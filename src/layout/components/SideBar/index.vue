9923ffda5146eb86f5b3506307bdb4b41a0d1d47
zack zheng <zack_zhengxiyun@163.com>
2023-10-31 19:44:32 +0800

    style(style): try v-bind in css

--- src/layout/components/SideBar/index.vue
+++ src/layout/components/SideBar/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="">
     <div class="logo-container">
-      <el-avatar size="44" shape="square" :src="logo"></el-avatar>
+      <el-avatar :size="logoHeight" shape="square" :src="logo"></el-avatar>
       <h1 class="logo-title" v-if="$store.getters.sidebarOpened">
         vue3-admin-front
       </h1>
@@ -15,10 +15,13 @@
 <script setup>
 import SidebarMenu from './SidebarMenu.vue'
 import logo from '@/assets/logo.png'
+
+const logoHeight = 44
 </script>
 
 <style lang="scss" scoped>
 .logo-container {
+  // height: v-bind(logoHeight) + 'px'; 似乎不生效
   height: 44px;
   padding: 10px 0 22px 0;
   display: flex;
