0af035ec2fd8333dd7bf50f87b8d330c01eca0b1
zack zheng <zack_zhengxiyun@163.com>
2023-11-30 22:14:42 +0800

    feat(function): update

--- src/layout/components/NavBar.vue
+++ src/layout/components/NavBar.vue
@@ -3,6 +3,8 @@
     <Hamburger />
     <Breadcrumb class="bread-container"/>
     <div class="right-menu">
+      <!-- 全屏切换 -->
+      <screen-full class="right-menu-item hover-effect" />
       <!-- 主题颜色切换 -->
       <theme-select class="right-menu-item hover-effect" />
       <!-- 语言切换 -->
@@ -39,6 +41,7 @@ import Hamburger from '@/components/hamburger/index'
 import Breadcrumb from '@/components/Breadcrumb/index'
 import LangSelect from '@/components/LangSelect/index'
 import ThemeSelect from '@/components/ThemeSelect/index'
+import ScreenFull from '@/components/ScreenFull/index'
 const store = useStore()
 const logout = () => {
   store.dispatch('user/logout')
